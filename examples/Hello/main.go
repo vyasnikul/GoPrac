f067150bf3772efa5964f06349e02294b9059e5b
vyasnikul <vyasnikul@ymail.com>
2019-03-18 18:51:52 -0700

    Algorithms

--- examples/Hello/main.go
+++ examples/Hello/main.go
@@ -4,13 +4,38 @@ import (
 	"fmt"
 )
 
-const (
-	name = "Hello, My Name is Nikul."
-)
+func linearSearch(arr []int, key int) bool {
+	for i, val := range arr {
+		if val == key {
+			fmt.Println(i, ":", val)
+			return true
+		}
+	}
+	return false
+}
+
+func binarySearch(arr []int, key int) bool {
+	min := 0
+	max := len(arr) - 1
+	median := 0
+	for min <= max {
+		median = (min + max) / 2
+		if arr[median] < key {
+			min = median + 1
+		} else {
+			max = median - 1
+		}
+	}
+	if min == len(arr) || arr[min] != key {
+		return false
+	}
+	fmt.Println(median, ":", key)
+	return true
+}
 
 func main() {
-	var name string
-	fmt.Print("Enter your name:")
-	fmt.Scanf("%s", &name)
-	fmt.Println("Hello my name is: ", name)
+	xs := []int{95, 78, 46, 58, 45, 86, 99, 251, 320}
+	fmt.Println("Input Array: ", xs)
+	fmt.Println("Linear Search Output:", linearSearch(xs, 99))
+	fmt.Println("Binary Search Output:", binarySearch(xs, 86))
 }
